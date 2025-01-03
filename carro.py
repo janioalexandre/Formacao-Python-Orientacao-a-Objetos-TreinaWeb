1eacdf897b9c8a90a097b5fcf951aad187274d84
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-01-03 08:51:51 -0300

    Classes e objetos

---
+++ carro.py
@@ -0,0 +1,31 @@
+class Carro():
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
+        self.cor = cor
+        self.qtd_portas = qtd_portas
+        self.tipo_combustivel = tipo_combustivel
+        self.potencia = potencia
+        self.qtd_combustivel = qtd_combustivel
+        self.is_ligado = is_ligado
+        self.velocidade = velocidade
+
+    def abastecer(self, qtd_combustivel):
+        self.qtd_combustivel += qtd_combustivel
+    
+    def ligar (self):
+        if self.is_ligado:
+            print('O carro já está ligado')
+        else:
+            self.is_ligado = True
+            print('O carro foi ligado')
+            
+    def desligado(self):
+        if self.is_ligado == False:
+            print('O carro já está desligado')
+        else:
+            self.is_ligado = False
+
+    def acelerar(self, velocidade=10):
+        if self.is_ligado:
+            self.velocidade += velocidade
+        else:
+            print('O carro precisa estar ligado para acelerar')
