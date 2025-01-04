a0c392d78a63e0ae03efd48cf5fce92989e3459d
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-01-04 20:46:14 -0300

    Construtores e destrutores

--- carro.py
+++ carro.py
@@ -1,14 +1,19 @@
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
+    """Essa é a classe carro, Essa classe é utilizada para instanciar novos carros em nosso programa"""
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
-        self.qtd_combustivel = qtd_combustivel
-        self.is_ligado = is_ligado
-        self.velocidade = velocidade
+        self.qtd_combustivel = 0
+        self.is_ligado = 0
+        self.velocidade = 0
+
+    def __del__(self):
+        print('O objeto foi removido da memória. :)')
 
     def abastecer(self, qtd_combustivel):
+        """O método abastecer recebe como parêmetro a quantidade de combustível e incrementa no atributo qtd_combustivel do objeto carro"""
         self.qtd_combustivel += qtd_combustivel
     
     def ligar (self):
--- classes_e_objetos.py
+++ classes_e_objetos.py
@@ -1,8 +1,11 @@
 import carro
 
-uno_vermelho = carro.Carro('preto', 4, 'gasolina', 2.0, 0, False, 0)
+uno_vermelho = carro.Carro('preto', 4, 'gasolina', 2.0)
+# help(carro.Carro)
+# help(uno_vermelho.abastecer)
 uno_vermelho.ligar()
 uno_vermelho.abastecer(10)
 uno_vermelho.acelerar()
 print(f"A quantidade de combustível no carro é {uno_vermelho.qtd_combustivel}")
 print(f"A velocidade do carro é {uno_vermelho.velocidade}")
+del uno_vermelho
