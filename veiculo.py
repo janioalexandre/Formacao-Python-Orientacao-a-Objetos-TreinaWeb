75e2811bfb54b3ff201ee141febb2cbd0b56f225
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-01-08 23:59:13 -0300

    Atributos de visibilidade e encapsulamento

--- veiculo.py
+++ veiculo.py
@@ -1,12 +1,12 @@
 class Veiculo():
     """Essa é a classe carro, Essa classe é utilizada para instanciar novos carros em nosso programa"""
     def __init__(self, cor, tipo_combustivel, potencia):
-        self.cor = cor
-        self.tipo_combustivel = tipo_combustivel
-        self.potencia = potencia
-        self.qtd_combustivel = 0
-        self.is_ligado = 0
-        self.velocidade = 0
+        self.__cor = cor
+        self.__tipo_combustivel = tipo_combustivel
+        self.__potencia = potencia
+        self.__qtd_combustivel = 0
+        self.__is_ligado = 0
+        self.__velocidade = 0
 
     def __del__(self):
         print('O objeto foi removido da memória. :)')
