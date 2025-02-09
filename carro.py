8d52f13f8830fe56c008e90a1d7984c8e14f4eb8
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-02-09 20:45:14 -0300

    Métodos que recebem argumentos

--- carro.py
+++ carro.py
@@ -1,15 +1,15 @@
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado):
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
         self.qtd_combustivel = qtd_combustivel
         self.is_ligado = is_ligado
+        self.velocidade = velocidade
     
     def abastecer(self, qtd_combustivel):
         self.qtd_combustivel += qtd_combustivel
-        return self.qtd_combustivel
 
     def ligar(self):
         if self.is_ligado:
@@ -24,3 +24,9 @@ class Carro():
         else:
             self.is_ligado = False
             print('O carro foi desligado')
+    
+    def acelerar(self, velocidade=10):
+        if self.is_ligado:
+            self.velocidade += velocidade
+        else:
+            print('O carro precisa estar ligado para acelerar')
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import carro
 
-prisma_vermelho = carro.Carro('vermelho', 4, 'flex', 1.0, 0, False)
+prisma_vermelho = carro.Carro('vermelho', 4, 'flex', 1.0, 0, False, 0)
 prisma_vermelho.ligar()
 prisma_vermelho.abastecer(20)
 prisma_vermelho.abastecer(10)
 print(f"A quantidade de combustível do carro é: {prisma_vermelho.qtd_combustivel}")
 
-onix_preto = carro.Carro('preto', 4, 'flex', 1.0, 0, False)
-onix_preto.desligar()
+onix_preto = carro.Carro('preto', 4, 'flex', 1.0, 0, False, 0)
+onix_preto.ligar()
+onix_preto.acelerar()
+print(f"A velocidade do carro é: {onix_preto.velocidade}")
