2a8c680fa1b1bd4b907d677eed301cbfaafdf772
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-02-10 10:24:23 -0300

    Construtores e Destrutores

--- carro.py
+++ carro.py
@@ -1,14 +1,19 @@
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velocidade):
+    """Essa é a classe Carro. Esta classe é utilizada para instanciar novos carros em nosso sistema."""
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
-        self.qtd_combustivel = qtd_combustivel
-        self.is_ligado = is_ligado
-        self.velocidade = velocidade
+        self.qtd_combustivel = 0
+        self.is_ligado = False
+        self.velocidade = 0
     
+    def __del__(self):
+        print('O objeto foi removido da memória. :)')
+
     def abastecer(self, qtd_combustivel):
+        """O método abastecer recebe como parâmetro a quantidade de combustível que será abastecida no carro."""
         self.qtd_combustivel += qtd_combustivel
 
     def ligar(self):
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import carro
 
-prisma_vermelho = carro.Carro('vermelho', 4, 'flex', 1.0, 0, False, 0)
+prisma_vermelho = carro.Carro('vermelho', 4, 'flex', 1.0)
 prisma_vermelho.ligar()
-prisma_vermelho.abastecer(20)
+help(prisma_vermelho.abastecer)
+# help(carro.Carro)
 prisma_vermelho.abastecer(10)
 print(f"A quantidade de combustível do carro é: {prisma_vermelho.qtd_combustivel}")
+del prisma_vermelho
 
-onix_preto = carro.Carro('preto', 4, 'flex', 1.0, 0, False, 0)
+onix_preto = carro.Carro('preto', 4, 'flex', 1.0)
 onix_preto.ligar()
 onix_preto.acelerar()
 print(f"A velocidade do carro é: {onix_preto.velocidade}")
